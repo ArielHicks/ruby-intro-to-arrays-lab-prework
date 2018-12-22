5395a2b875de656722293f2a52cab952a5a69535
Ariel Hicks <hicks.ariel@gmail.com>
2018-12-22 00:52:21 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,23 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  [1, 2]
+end
+
+def first_element(array)
+  array.index(1)
+end
+
+def third_element(array)
+  array.index(3)
+end
+
+def last_element(array)
+  array.last
+end
+
+def first_element_with_array_methods(array)
+
+end
